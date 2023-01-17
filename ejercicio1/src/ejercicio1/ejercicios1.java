6863f3936f2dd5594990034eadeddd49ffeacc5c
Jcastellorequena <josecastellojob@gmail.com>
2023-01-17 20:10:23 +0100

    Update ejercicios1.java

--- ejercicio1/src/ejercicio1/ejercicios1.java
+++ ejercicio1/src/ejercicio1/ejercicios1.java
@@ -3,6 +3,29 @@ package ejercicio1;
 public class ejercicios1 {
 
 	
+	public static void sumaNumeros() {
+		int numero = 36;
+		int numero2 = 27;
+		int suma = numero+numero2;
+		System.out.println("La suma es: " + suma );
+	
+	}
+	public static void variableNumeros() {
+		int numero = 15;
+		int numero2 = 4;
+		int suma = numero + numero2;
+		int resta = numero - numero2;
+		int division = (int) numero/numero2;
+		System.out.println("15+4: "  + suma);
+		System.out.println("15-4: " + resta);
+		System.out.println("15/4: " + division);
+		System.out.println("15*4: "+ numero*numero2);
+		System.out.println("15%4: " + numero%numero2);
+		
+		
+	}
+	
+	
 	public static void comparaNumeros () {
 		int x=10;
 		int y=10;
@@ -24,7 +47,8 @@ public class ejercicios1 {
 	
 	public static void main(String[] args) {
 		comparaNumeros();
-		
+		sumaNumeros();
+		variableNumeros();
 
 	}
 
